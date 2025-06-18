bc17c10f8a3fffcd27318221393533d7da53bbba
Raf <rafay.b075@gmail.com>
2025-06-17 22:37:43 -0500

    Added status codes

--- backend/controllers/planner.js
+++ backend/controllers/planner.js
@@ -10,16 +10,22 @@ const getBestClasses = async (req, res) => {
             parsed[sem][i].title = parsed[sem][i].title.trim() // would be a good idea to sanitize the string
 
             const course = await Course.find({"info.department": courses.dept, "info.number": courses.course}) // find the identified course
+            // might want to switch this out, some might be taking classes that not exist anymore or newer classes not in database
+            if(!course) { 
+                return res.status(404).json({error: `Class ${courses.dept} ${courses.course} not found.`});
+            }
             const sections = course.sections
             const professorNames = sections.map(x => x.prof)
 
             const professors = await Professor.find({"info.name": {$in: professorNames}}) // find the entire list of professors that teach the course
-            
+            if(!course) { 
+                return res.status(404).json({error: `No Professors Found`});
+            }
             professors.sort((a, b) => (a.info.averageGPA + a.info.averageRating) - (b.info.averageGPA + b.info.averageRating)) // sort by a combination of average GPA + rating
             parsed[sem][i].professors = professors // add to parsed info
         })
     })
-    return parsed
+    return res.status(200).json(parsed);
 }
 
 module.exports = { getBestClasses };
