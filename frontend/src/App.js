d34b00d8d0294e9a8caea83ab4b346fcae66589d
Raf <rafay.b075@gmail.com>
2025-07-05 12:18:28 -0500

    Landing Page
    Need to add changes to finalize pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 //libraries
-import { useEffect, useState } from "react";
 import React from "react";
+import Landing from "./components/Landing";
 
 //components
 import Navbar from "./components/Navbar";
@@ -13,6 +13,51 @@ import Home from "./pages/Home";
 import SearchResults from "./pages/SearchResults";
 
 const App = () => {
+  /*
+   const [professors, setProfessors] = useState([]);
+
+    useEffect(() => {
+        // const fetchProfessors = async () => {
+        //     const response = await fetch ("/server/api/professors/CSCE/120", {
+        //         method: "GET"
+        //     });
+        //     const profJson = await response.json();
+        //     console.log("profJson: ", profJson);
+
+
+        //     if(response.ok){
+        //         setProfessors(profJson);
+        //     }
+        // }
+        // fetchProfessors();
+
+        const professorObj1 = {
+             info: {
+                name: "professor1Name", 
+                averageGPA: 3.58,
+                totalSections: 12,
+                totalStudents: 144,
+                averageRating: 4.23,
+                totalRatings: 15, 
+                yearsTaught: 12,   
+            },
+            courses: ["courseId1","courseId2"]
+        };
+        const professorObj2 = {
+             info: {
+                name: "professor2Name", 
+                averageGPA: 3.59,
+                totalSections: 13,
+                totalStudents: 124,
+                averageRating: 4.13,
+                totalRatings: 1123, 
+                yearsTaught: 1212,
+            },
+            courses: ["courseId1", "courseId2"]
+        };
+        const professorData = [professorObj1, professorObj2];
+        setProfessors(professorData);
+    }, []);
 
     return (
         <div className = "App bg-black text-white h-screen relative ">
@@ -34,6 +79,9 @@ const App = () => {
         </div>
     );
 }
+    */
+  return <Landing />;
+};
 
 
 export default App;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,35 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js,jsx}",
+    "./src/index.js",
+    "./src/components/Wave.js",
     "./public/index.html"
   ],
   theme: {
     extend: {
+    
       colors: {
-        maroon: "#800000"
-      }
+        maroon: "#500000",
+        blackX: "#141414"
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      keyframes: {
+        waveRotate: {
+          '0%': {
+            transform: 'translate(-50%, -75%) rotate(0deg)',
+          },
+          '100%': {
+            transform: 'translate(-50%, -75%) rotate(360deg)',
+          },
+        },
+      },
+      animation: {
+        wave1: 'waveRotate 5s linear infinite',
+        wave2: 'waveRotate 10s linear infinite',
+        wave3: 'waveRotate 15s linear infinite',
+      },
     },
   },
   plugins: [],
