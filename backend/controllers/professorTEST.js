76b1ae6823e94e802837a6953f6993f76dd5142d
TheKillerKetchup <48397623+TheKillerKetchup@users.noreply.github.com>
2025-09-04 09:55:00 -0600

    tests

--- backend/controllers/professorTEST.js
+++ backend/controllers/professorTEST.js
@@ -12,9 +12,13 @@ const getProfessorInfoById = async (req, res) => {
     const { professorID } = req.query;
     const client = await pgPool.connect();
 
+    // console.log(professorID);
+    // console.log(client);
     try {
-        const sql = `SELECT * FROM course_explorer.professors WHERE professor_id = $1`;
+        const sql = `SELECT * FROM course_explorer.professors WHERE id = $1`;
+        // console.log(sql);
         const result = await client.query(sql, [professorID]);
+        // console.log(result);
         if (!result.rows.length) {
             return res.status(404).json({ error: `Professor with ID ${professorID} does not exist.` });
         }
@@ -33,4 +37,39 @@ const getProfessorInfoById = async (req, res) => {
     }
 }
 
-module.exports = {getProfessorInfoById};
+const getProfessorsByCourse = async (req, res) => {
+    
+};
+const getProfessorByName = async (req, res) => {
+    const { profName } = req.params;
+    const client = await pgPool.connect();
+    
+    try {
+        const sql = `SELECT * FROM course_explorer.professors WHERE name = $1`;
+        const result = await client.query(sql, [profName]);
+        if (!result.rows.length) {
+            return res.status(404).json({ error: `Professor with name ${profName} not found.` });
+        }
+        const professor = result.rows[0];
+        if(!professor){
+            return res.status(404).json({error: `Professor with name ${profName} not found.`});
+        }
+    } catch (error) {
+        return res.status(500).json({ error: "Server error while fetching professor data."});
+    } finally {
+        client.release();
+    }
+
+    
+    return res.status(200).json(professor);
+};
+const getProfessorByCourseAndName = async (req, res) => {
+
+};
+
+module.exports = {
+    getProfessorInfoById,
+    getProfessorsByCourse, 
+    getProfessorByName, 
+    getProfessorByCourseAndName
+};
--- backend/test/professor.test.js
+++ backend/test/professor.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const { expect } = require('chai');
+const assert = require('chai').assert;
 const app = require('../server'); 
 
 describe("Professor API", () => {
@@ -9,15 +10,18 @@ describe("Professor API", () => {
         expect(res.body).to.have.property("error");
     });
 
-    // it('GET /professorInfo?<params> should return professor Info', async () => {
-    //     const res = await request(app).get(`/api/professorTEST/professorInfo?professorID=100615`);
-    //     try{
-    //         expect(res.status).to.equal(200);
-    //         // expect(res.body).to.be.an("object");
-    //         expect(res.body.length).to.be.greaterThan(0);
-    //     }catch(error){
-    //         console.log("Test failed. Response was: ", res.status, res.body);
-    //         throw error;
-    //     }
-    // });
+    it('GET /professorInfo?<params> should return professor Info', async () => {
+        const res = await request(app).get(`/api/professorTEST/professorInfo?professorID=100615`);
+        try{
+            // console.log(Object.keys(res.body).length);
+            expect(res.status).to.equal(200);
+            expect(res.body).to.be.an("object");
+            expect(Object.keys(res.body).length).to.be.greaterThan(0);
+        }catch(error){
+            console.log("Test failed. Response was: ", res.status, res.body);
+            throw error;
+        }
+    });
 });
+
+describe("Professor API ")
