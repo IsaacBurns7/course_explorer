db488438c39a85bba152570ca160c254cebfb45a
Raf <rafay.b075@gmail.com>
2025-12-20 22:27:45 -0600

    course_explorer.users instead of just users

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,7 @@ const { neon } = require('@neondatabase/serverless');
 
 //this is for running the database locally
 
+/*
 const pool = new Pool({
   host: "localhost",
   user: process.env.DB_USERNAME,
@@ -10,17 +11,17 @@ const pool = new Pool({
   database: process.env.DB_NAME, //process.env.DB_NAME,
   port: process.env.PGPORT || 5432,
 });
-
+*/
 //running db via neon
 
 // console.log("Global setup: Attempting to connect to DB at ", process.env.NEON_DB_URL);
 
-// const pool = new Pool({
-//     connectionString: process.env.NEON_DB_URL,
-//     ssl: {
-//         require: true
-//     }
-// });
+const pool = new Pool({
+     connectionString: process.env.NEON_DB_URL,
+    ssl: {
+        require: true
+    }
+ });
 
 pool.connect()
   .then(() => {
