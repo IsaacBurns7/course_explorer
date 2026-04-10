9fd98b62fc84e7e82b890a16584e34226b93ef8c
Raf <rafay.b075@gmail.com>
2026-04-09 19:40:04 -0500

    revert commit!

--- backend/src/routes/scheduler.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { requireAuth } = require('../middleware/auth');
-const schedulerController = require('../controllers/schedule_backup/saver');
-
-// POST /api/scheduler/save
-router.post('/save', requireAuth, schedulerController.saveSchedule);
-
-module.exports = router;
